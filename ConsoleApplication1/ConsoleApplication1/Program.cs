03fd0fb59b03a82946721fe9f3a9e10e83a86b30
alonso <alonsobugs@gmail.com>
2016-09-17 16:31:22 -0600

    Creación de la aplicación de Hello World componentizada

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static string GenerateHelloWorld (string userName)
+        {
+            string result;
+            result = "Hello World, " + userName;
+            return (result);
+        }
+        
+        static void Main(string[] args)
+        {
+
+            string userName;
+            Console.WriteLine("Escriba el nombre de usuario: ");
+            userName = Console.ReadLine();
+            Console.WriteLine(GenerateHelloWorld(userName));
+            Console.ReadLine();
+
+        }
+    }
+}
