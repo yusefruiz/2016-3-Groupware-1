bfe24b74641d198c49710943e3d504c48f87d0c6
hecferme <hecferme@gmail.com>
2016-10-08 16:31:08 -0600

    ejemplos wcf

---
+++ ConsoleApplication1/Hangman.UnitTestsWcf/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hangman.UnitTestsWcf
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // declare una variable para acceder al wcf
+            var hangmanWcf = new WcfHangmanTools.HangmanToolsClient();
+            var elResultado = hangmanWcf.SumarCantidades(10, 20);
+
+            Assert.AreEqual(30, elResultado);
+        }
+    }
+}
