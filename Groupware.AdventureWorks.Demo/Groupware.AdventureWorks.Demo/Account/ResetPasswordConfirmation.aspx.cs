8a3d8ffa5ab883aed3506ced4112289beca44972
hecferme <hecferme@gmail.com>
2016-10-15 16:08:49 -0600

    se crea proyecto para acceso a base de datos con entity framework

---
+++ Groupware.AdventureWorks.Demo/Groupware.AdventureWorks.Demo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Groupware.AdventureWorks.Demo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Groupware.AdventureWorks.Demo/Groupware.AdventureWorks.Demo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Groupware.AdventureWorks.Demo.Startup))]
+namespace Groupware.AdventureWorks.Demo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
